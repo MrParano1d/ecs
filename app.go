c2840de9856cdb5caddf07a85f9953d4280ed263
MrParano1d <jp.s@live.de>
2022-04-17 23:23:15 +0200

    fix(scheduler): :bug: free scheduler from memory

--- app.go
+++ app.go
@@ -105,6 +105,7 @@ func (a *App) Run() error {
 		for _, stage := range a.stages.GetOrderedStages() {
 			scheduler := NewScheduler(stage)
 			scheduler.RunSystems(a.world, a.events)
+			scheduler = nil
 		}
 
 		reader := NewEventReader(a.Events()[AppExitEvent{}])
